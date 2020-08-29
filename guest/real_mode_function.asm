ff14ef1a3b83b5e24ff4fdb10fd843f885208dcc
amiryeshooroon <amiryeshooroon@gmail.com>
2020-08-29 17:03:02 +0300

    Set up IVT, add 16bit real mode flow and return back to long mode

--- guest/real_mode_function.asm
+++ guest/real_mode_function.asm
@@ -5,27 +5,48 @@
 %define BOOTABLE_SIGNATURE 0x55aa
 %define REAL_MODE_OUTPUT_BUFFER_ADDRESS 0x2200
 %define REAL_MODE_CODE_START 0x4200
-%define SavedStackAddress 0x6000
+%define SAVED_STACK_ADDRESS 0x6000
+%define IVT_ADDRESS 0x7500
 
 global DiskReader
 global AsmEnterRealModeRunFunction
 
 [BITS 64]
 AsmEnterRealModeRunFunction:
-    mov [SavedStackAddress], rsp
+    mov [SAVED_STACK_ADDRESS], rsp
     pushf
-    push 8
+    push 24
     iretq
-AsmEnterLongModeFromFunction:
+AsmReturnFromRealModeFunction:
     cli
     mov cs, 8
-    mov ds, 8
-    mov rsp, [SavedStackAddress]
+    mov ds, 16
+    mov ss, 16
+    mov rsp, [SAVED_STACK_ADDRESS]
+    ret
 
 [BITS 32]
 EnterRealMode:
-    SetUpInterruptVector:
-        ; define the interrupt vector for real mode
+    ; define the interrupt vector for real mode
+    mov eax, IVT_ADDRESS ; ivt
+    mov word [eax], 0xff ; limit
+    MovQwordToAddressLittleEndian IVT_ADDRESS + 2, 0x0, 0x0 ; ivt address (0)
+    lidt [IVT_ADDRESS]
+    jmp 32:(DisableLongMode - EnterRealMode + REAL_MODE_CODE_START) ; 16 bit code selector
+
+[BITS 16]
+DisableLongMode:
+    ; Disable long mode here, execute desired function
+    jmp 0:(EnterRealModeEnd - EnterRealMode + REAL_MODE_CODE_START)
+
+BackToLongMode:
+    ; Enable long mode when back from handling interrupts
+    jmp 24:(EnableProtectedMode - EnterRealMode + REAL_MODE_CODE_START)
+
+[BITS 32]
+EnableProtectedMode:
+    jmp 8:AsmReturnFromRealModeFunction
+
 EnterRealModeEnd:
 
 [BITS 16]
@@ -35,4 +56,5 @@ DiskReader:
     mov dl, [DAP_ADDRESS + 0x10]
     xor bx, bx
     int 13h
+    jmp 0:(BackToLongMode - EnterRealMode + REAL_MODE_CODE_START)
 DiskReaderEnd:
