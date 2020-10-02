90cda7e32d2688dbbcd8fc0e2223eceb1b1a99c5
Amir Yeshurun <amiryeshooroon@gmail.com>
2020-10-02 15:53:13 +0300

    Modify guest registers before resuming back

--- native-hypervisor/vmm/vmm_asm.asm
+++ native-hypervisor/vmm/vmm_asm.asm
@@ -26,7 +26,7 @@ SetupCompleteBackToGuestState:
     vmlaunch
     pushf
     pop rax
-    ret ; an error occured
+    ret ; An error occured
 
 VmmToVm:
     OutputSerial 'H'
@@ -48,15 +48,45 @@ HandleVmExit:
     mov qword [fs:96], r13
     mov qword [fs:104], r14
     mov qword [fs:112], r15
-    mov rax, 0x0000681c    ; geust RSP
+    mov rax, 0x0000681c    ; Geust RSP
     vmread rbx, rax
     mov qword [fs:120], rbx
-    mov rax, 0x0000681e    ; geust RIP
+    mov rax, 0x0000681e    ; Geust RIP
     vmread rbx, rax
     mov qword [fs:128], rbx
+
+    ; Run handler function
     call HandleVmExitEx
+
+    ; Change guest registers according to current state
+    mov rcx, qword [fs:16]
+    mov rdx, qword [fs:24]
+    mov rsi, qword [fs:32]
+    mov rdi, qword [fs:40]
+    mov rbp, qword [fs:48]
+    mov r8, qword [fs:56]
+    mov r9, qword [fs:64]
+    mov r10, qword [fs:72]
+    mov r11, qword [fs:80]
+    mov r12, qword [fs:88]
+    mov r13, qword [fs:96]
+    mov r14, qword [fs:104]
+    mov r15, qword [fs:112]
+
+    mov rax, 0x0000681c    ; geust RSP
+    mov rbx, [fs:120]
+    vmwrite rax, rbx
+    mov rax, 0x0000681e    ; geust RIP
+    mov rbx, [fs:128]
+    vmwrite rax, rbx
+
+    mov rax, qword [fs:0]
+    mov rbx, qword [fs:8]
+
+    ; Back to guest
     vmresume
-    OutputSerial 'F' ; This should never be executed
+
+   ; This should never be executed
 .assertHandleVmExit:
     OutputSerial 'F'
     jmp .assertHandleVmExit
