033b42c0c8c00dccec9414a7aa978c5c56bbd7e9
Amir Yeshurun <amiryeshooroon@gmail.com>
2020-10-30 19:45:31 +0200

    Migrate data ftom KPP module to SYSCALL handlers

--- native-hypervisor/include/vmx_modules/kpp_module.h
+++ native-hypervisor/include/vmx_modules/kpp_module.h
@@ -4,30 +4,23 @@
 #include <types.h>
 #include <vmm/vmm.h>
 #include <x86_64.h>
+#include <win_kernel/syscall_handlers.h>
 
 #define KPP_MODULE_MAX_COUNT 100
 
-typedef struct _PATCH_GAURD_ENTRY
-{
-    QWORD address;
-    // Max instruction length in x86 is 15 bytes
-    BYTE instruction[X86_MAX_INSTRUCTION_LEN];
-    BYTE instrutcionLength;
-} PATCH_GAURD_ENTRY, *PPATCH_GAURD_ENTRY;
-
-
 typedef struct _KPP_MODULE_DATA
 {
-    // First: Address, Second: Fake data
-    PATCH_GAURD_ENTRY savedAddresses[KPP_MODULE_MAX_COUNT];
-    QWORD savedAddressesCount;
+    PSYSCALL_DATA syscallsData;
+    QWORD hookedSyscalls[KPP_MODULE_MAX_COUNT];
+    QWORD hookedSyscallsCount;
 } KPP_MODULE_DATA, *PKPP_MODULE_DATA;
 
 STATUS KppHandleEptViolation(IN PCURRENT_GUEST_STATE data, IN PMODULE module);
-STATUS RegisterNewProtectedKppEntry(IN QWORD guestPhysicalAddress, IN BYTE_PTR instruction, IN BYTE instructionLength, IN PMODULE kppModule);
+STATUS RegisterNewProtectedKppEntry(IN QWORD syscallId, IN QWORD guestPhysicalAddress, IN BYTE_PTR instruction, 
+    IN BYTE instructionLength, IN PMODULE kppModule);
 STATUS KppModuleInitializeAllCores(IN PSHARED_CPU_DATA sharedData, IN PMODULE module, IN GENERIC_MODULE_DATA initData);
 STATUS KppModuleInitializeSingleCore(IN PSINGLE_CPU_DATA data);
-STATUS EmulatePatchGuardAction(IN PPATCH_GAURD_ENTRY kppEntries, IN QWORD count, IN QWORD address, IN BYTE instructionLength);
+STATUS EmulatePatchGuardAction(IN PKPP_MODULE_DATA kppData, IN QWORD address, IN BYTE instructionLength);
 STATUS KppHandleEptViolation(IN PCURRENT_GUEST_STATE data, IN PMODULE module);
 
 
--- native-hypervisor/vmx_modules/kpp_module.c
+++ native-hypervisor/vmx_modules/kpp_module.c
@@ -11,6 +11,8 @@ STATUS KppModuleInitializeAllCores(IN PSHARED_CPU_DATA sharedData, IN PMODULE mo
     PrintDebugLevelDebug("Starting initialization of KPP module for all cores\n");
     sharedData->heap.allocate(&sharedData->heap, sizeof(KPP_MODULE_DATA), &module->moduleExtension);
     SetMemory(module->moduleExtension, 0, sizeof(KPP_MODULE_DATA));
+    PKPP_MODULE_DATA extension = module->moduleExtension;
+    extension->syscallsData = &__ntDataStart;
     PrintDebugLevelDebug("Shared cores data successfully initialized for KPP module\n");
     return STATUS_SUCCESS;
 }
@@ -22,45 +24,114 @@ STATUS KppModuleInitializeSingleCore(IN PSINGLE_CPU_DATA data)
     return STATUS_SUCCESS;
 }
 
-STATUS RegisterNewProtectedKppEntry(IN QWORD guestPhysicalAddress, IN BYTE_PTR instruction, 
+STATUS RegisterNewProtectedKppEntry(IN QWORD idx, IN QWORD guestPhysicalAddress, IN BYTE_PTR instruction, 
     IN BYTE instructionLength, IN PMODULE kppModule)
 {
     PKPP_MODULE_DATA kppData = (PKPP_MODULE_DATA)kppModule->moduleExtension;
-    if(kppData->savedAddresses >= KPP_MODULE_MAX_COUNT)
+    if(kppData->hookedSyscallsCount >= KPP_MODULE_MAX_COUNT)
         return STATUS_NO_SPACE_AVAILABLE;
-    kppData->savedAddresses[kppData->savedAddressesCount].address = guestPhysicalAddress;
-    CopyMemory(kppData->savedAddresses[kppData->savedAddressesCount].instruction, instruction, 
+    kppData->syscallsData[kppData->hookedSyscalls[idx]].hookedInstructionAddress = guestPhysicalAddress;
+    kppData->syscallsData[kppData->hookedSyscalls[idx]].hookedInstructionLength = instructionLength;
+    CopyMemory(kppData->syscallsData[kppData->hookedSyscalls[idx]].hookedInstrucion, instruction, 
         instructionLength);
-    kppData->savedAddressesCount++;
     return STATUS_SUCCESS;
 }
 
-STATUS EmulatePatchGuardAction(IN PPATCH_GAURD_ENTRY kppEntries, IN QWORD count,
-        IN QWORD address, IN BYTE instructionLength)
+BOOL CheckIfAddressContainsInstruction(IN PKPP_MODULE_DATA kppData, IN QWORD address,
+    IN BYTE readLength, OUT BOOL* before, OUT PSYSCALL_DATA* entry, QWORD_PTR ext)
+{
+    for(QWORD i = 0; i < kppData->hookedSyscallsCount; i++)
+    {
+        if(address >= kppData->syscallsData[kppData->hookedSyscalls[i]].hookedInstructionAddress
+             && address <= kppData->syscallsData[kppData->hookedSyscalls[i]].hookedInstructionAddress
+              + kppData->syscallsData[kppData->hookedSyscalls[i]].hookedInstructionLength)
+        {
+            *before = FALSE;
+            *entry = &(kppData->syscallsData[kppData->hookedSyscalls[i]]);
+            *ext = address - kppData->syscallsData[kppData->hookedSyscalls[i]].hookedInstructionAddress;
+            return TRUE;
+        }
+        else if(address <= kppData->syscallsData[kppData->hookedSyscalls[i]].hookedInstructionAddress
+         && address + readLength >= kppData->syscallsData[kppData->hookedSyscalls[i]].hookedInstructionAddress)
+        {
+            *before = TRUE;
+            *entry = &(kppData->syscallsData[kppData->hookedSyscalls[i]]);
+            *ext = kppData->syscallsData[kppData->hookedSyscalls[i]].hookedInstructionAddress - address;
+            return TRUE;
+        }
+    }
+    
+    return FALSE;
+}
+
+VOID BuildKppResult(OUT PVOID val, IN QWORD guestPhysical, IN QWORD readLength, 
+    IN PKPP_MODULE_DATA kppData)
+{
+    QWORD hostVirtualAddress = TranslateGuestPhysicalToHostVirtual(guestPhysical), ext;
+    PSYSCALL_DATA entry;
+    BOOL isBefore;
+    if(CheckIfAddressContainsInstruction(kppData, guestPhysical, readLength, &isBefore, 
+        &entry, &ext))
+    {
+        // The hidden instruction is a prefix of the current checked instruction
+        if(!isBefore)
+        {
+            // ext = offset from the beggining of the hidden instruction
+            CopyMemory(val, entry->hookedInstrucion + ext, entry->hookedInstructionLength - ext 
+                <= readLength ? entry->hookedInstructionLength - ext : readLength);
+            if(entry->hookedInstructionLength - ext < readLength)
+                CopyMemory((BYTE_PTR)val + (entry->hookedInstructionLength - ext), 
+                    TranslateGuestPhysicalToHostVirtual(guestPhysical + entry->hookedInstructionLength 
+                        - ext), readLength - (entry->hookedInstructionLength - ext));
+        }
+        // The hidden instruction is a suffix of the current checked instruction
+        else
+        {
+            // ext = the number of bytes before the beggining of the hidden instruction
+            CopyMemory(val, hostVirtualAddress, ext);
+            CopyMemory((BYTE_PTR)val + ext, entry->hookedInstrucion, readLength - ext);
+        }      
+    }
+    else
+        CopyMemory(val, hostVirtualAddress, readLength);
+}
+
+STATUS EmulatePatchGuardAction(IN PKPP_MODULE_DATA kppData, IN QWORD address, IN BYTE instructionLength)
 {
     PREGISTERS regs = &(GetVMMStruct()->guestRegisters);
     BYTE inst[X86_MAX_INSTRUCTION_LEN];
+    BOOL isProtectedAddress, isBefore;
+    QWORD ext;
+    PSYSCALL_DATA entry;
+
     CopyGuestMemory(inst, regs->rip, instructionLength);
     if(instructionLength == 3 && inst[0] == 0x41 && inst[1] == 0x8b && inst[2] == 0x02)
     {
         // mov eax,DWORD PTR [r10]
         DWORD val;
-        CopyGuestMemory(&val, regs->r10, sizeof(DWORD));
+        BuildKppResult(&val, address, 4, kppData);
         regs->rax = (regs->rax & 0xffffffff00000000ULL) | val;
     }
     else if(instructionLength == 3 && inst[0] == 0x41 && inst[1] == 0x8a && inst[2] == 0x02)
     {
         // mov al,BYTE PTR [r10]
         BYTE val;
-        CopyGuestMemory(&val, regs->r10, sizeof(BYTE));
+        BuildKppResult(&val, address, 1, kppData);
         regs->rax = (regs->rax & 0xffffffffffffff00ULL) | val;
     }
     else if(instructionLength == 4 && inst[0] == 0x41 && inst[1] == 0x0f && inst[2] == 0xb7 && inst[3] == 0x02)
     {
         // movzx eax,WORD PTR [r10]
         WORD val;
-        CopyGuestMemory(&val, regs->r10, sizeof(WORD));
+        BuildKppResult(&val, address, 2, kppData);
         regs->rax = (regs->rax & 0xffffffffffff0000ULL) | val;
+    }
+    else if(instructionLength == 3 && inst[0] == 0x49 && inst[1] == 0x8b && inst[2] == 0x01)
+    {
+        // mov rax,QWORD PTR [r9]
+        QWORD val;
+        BuildKppResult(&val, address, 8, kppData);
+        regs->rax = val;
     }
     else
     {
@@ -75,6 +146,5 @@ STATUS KppHandleEptViolation(IN PCURRENT_GUEST_STATE data, IN PMODULE module)
 {
     QWORD address = vmread(GUEST_PHYSICAL_ADDRESS), instructionLength = vmread(VM_EXIT_INSTRUCTION_LEN);
     PKPP_MODULE_DATA kppData = (PKPP_MODULE_DATA)module->moduleExtension;
-    return EmulatePatchGuardAction(kppData->savedAddresses, kppData->savedAddressesCount,
-        address, instructionLength);
+    return EmulatePatchGuardAction(kppData, address, instructionLength);
 }
