5effe41546fcd386dd024473e0517a40e47236d1
amiryeshurun <amiryeshooroon@gmail.com>
2020-11-05 19:31:46 +0200

    Complete hookig mechanism

--- native-hypervisor/include/vmx_modules/syscalls_module.h
+++ native-hypervisor/include/vmx_modules/syscalls_module.h
@@ -5,8 +5,13 @@
 #include <vmm/vmm.h>
 #include <vmx_modules/module.h>
 #include <win_kernel/syscall_handlers.h>
+#include <utils/map.h>
 
+// Used to determine how many vm-exits should occur before hooking the system calls
 #define COUNT_UNTIL_HOOK 1000
+
+// Used to determine if a physical address is assosiated with a return event or not
+#define RETURN_EVENT_FLAG (1 << 50)
 
 typedef struct _SYSCALLS_MODULE_EXTENSION
 {
@@ -18,6 +23,7 @@ typedef struct _SYSCALLS_MODULE_EXTENSION
     BYTE_PTR win32k;
     BYTE_PTR lstar;
     QWORD guestCr3;
+    QWORD_MAP addressToSyscall;
 } SYSCALLS_MODULE_EXTENSION, *PSYSCALLS_MODULE_EXTENSION;
 
 STATUS SyscallsModuleInitializeAllCores(IN PSHARED_CPU_DATA sharedData, IN PMODULE module, IN PGENERIC_MODULE_DATA initData);
@@ -27,6 +33,7 @@ STATUS SyscallsHandleMsrWrite(IN PCURRENT_GUEST_STATE data, IN PMODULE module);
 STATUS SyscallsHandleException(IN PCURRENT_GUEST_STATE data, IN PMODULE module);
 STATUS LocateSSDT(IN BYTE_PTR lstar, OUT BYTE_PTR* ssdt, IN QWORD guestCr3);
 VOID GetSystemTables(IN BYTE_PTR ssdt, OUT BYTE_PTR* ntoskrnl, OUT BYTE_PTR* win32k, IN QWORD guestCr3);
-STATUS HookSystemCalls(IN QWORD guestCr3, IN BYTE_PTR ntoskrnl, IN BYTE_PTR win32k, IN QWORD count, ...);
+STATUS HookSystemCalls(IN PMODULE module, IN QWORD guestCr3, IN BYTE_PTR ntoskrnl, IN BYTE_PTR win32k, 
+    IN QWORD count, ...);
 
 #endif
