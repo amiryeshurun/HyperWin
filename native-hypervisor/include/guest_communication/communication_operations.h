0980220e1ecd80107c04e34cc6dcb20161f91e6c
Amir Yeshurun <amiryeshooroon@gmail.com>
2020-11-16 14:08:25 +0200

    Add an operation to protect the current process

--- native-hypervisor/include/guest_communication/communication_operations.h
+++ native-hypervisor/include/guest_communication/communication_operations.h
@@ -6,7 +6,8 @@
 /* Communication block different operations */
 typedef QWORD OPERATION, *POPERATION;
 
-#define OPERATION_INIT      0x4857494e4954 // HWINIT ASCII
-#define OPERATION_COMPLETED 0x444f4e45     // DONE ASCII
+#define OPERATION_INIT              0x4857494e4954    // HWINIT ASCII
+#define OPERATION_PROTECTED_PROCESS 0x70726f74656374  // PROTECT ASCII
+#define OPERATION_COMPLETED         0x444f4e45        // DONE ASCII
 
 #endif
--- native-hypervisor/include/win_kernel/process.h
+++ native-hypervisor/include/win_kernel/process.h
@@ -8,9 +8,16 @@ enum
 {
     PS_PROTECTED_SYSTEM = 0x72,
     PS_PROTECTED_WINTCB = 0x62,
+    PS_PROTECTED_WINTCB_LIGHT = 0x61,
     PS_PROTECTED_NONE = 0x0
 };
 
-STATUS MarkProcessProtected(IN QWORD eprocess);
+enum
+{
+    PS_PROTECTED_SIGNER_WINTCB = 0x6
+};
+
+STATUS MarkProcessProtected(IN QWORD eprocess, IN BYTE protectionLevel, IN BYTE signLevel, 
+    IN BYTE sectionSignLevel);
 
 #endif
